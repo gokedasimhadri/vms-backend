8015ee4250a7dd0d7dd52de812ee7098a880b874
srikar <srikarm@adityauniversity.in>
2026-09-22 09:37:56 +0530

    backend

--- modules/staff/staff.controller.js
+++ modules/staff/staff.controller.js
@@ -45,10 +45,7 @@ exports.getStaffData = async (req, res) => {
     const docs = await db.collection(collName)
       .find(query)
       .sort({ _id: -1 })
-<<<<<<< Updated upstream
       .limit(1000)
-=======
->>>>>>> Stashed changes
       .toArray();
 
     return res.json({
